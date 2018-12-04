af7d1974c9bd0b0fdb392781ffe37e6b20548943
Ignas Asmontas <ignas.asmontas@gmail.com>
2018-12-04 22:14:14 +0200

    Fixed ngw piece

--- vpc.tf
+++ vpc.tf
@@ -7,9 +7,15 @@ locals {
   generic_tag  = "${var.owner}-${terraform.workspace}"
   subnet_count = 4
 
-  # IPs per subnet = subnet_size - 5; 
-  # Therefore actually available IPs: 11
+  /*
+    IPs per subnet = subnet_size - 5; 
+    Therefore actually available IPs: 11
+  */
   subnet_size = 16
+
+
+  cidr_vpc_mask = "${32 - log("${local.subnet_size * local.subnet_count}", 2)}"
+  cidr_subnet_mask = "${32 - log("${local.subnet_size}", 2)}"
 
   # Divisor for separating subnet types, e.g. public and private
   divisor = 2
@@ -24,7 +30,7 @@ locals {
 // A slice of network for each participant
 resource "aws_vpc" "main" {
   # variable for participant: 10.0.0.0; 10.0.0.64; 10.0.0.128
-  cidr_block = "10.0.0.0/26"
+  cidr_block = "10.0.0.0/${local.cidr_vpc_mask}"
 
   tags {
     Name = "${local.generic_tag}"
@@ -39,10 +45,11 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+// Public type subnets for LB
 resource "aws_subnet" "public" {
   count                   = "${local.public_subnet_count}"
   vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "10.0.0.${count.index * local.subnet_size}/28"
+  cidr_block              = "10.0.0.${count.index * local.subnet_size}/${local.cidr_subnet_mask}"
   map_public_ip_on_launch = true
   availability_zone       = "${element("${local.azs}", "${count.index}")}"
 
@@ -51,10 +58,11 @@ resource "aws_subnet" "public" {
   }
 }
 
+// Private type subnets for compute
 resource "aws_subnet" "private" {
   count                   = "${local.private_subnet_count}"
   vpc_id                  = "${aws_vpc.main.id}"
-  cidr_block              = "10.0.0.${(count.index + local.divisor) * local.subnet_size }/28"
+  cidr_block              = "10.0.0.${(count.index + local.divisor) * local.subnet_size }/${local.cidr_subnet_mask}"
   map_public_ip_on_launch = false
   availability_zone       = "${element("${local.azs}", "${count.index}")}"
 
@@ -64,6 +72,7 @@ resource "aws_subnet" "private" {
 }
 
 resource "aws_eip" "nat" {
+  count = "${local.public_subnet_count}"
   vpc = true
 
   tags {
@@ -71,13 +80,13 @@ resource "aws_eip" "nat" {
   }
 }
 
-resource "aws_nat_gateway" "gw" {
-  count = 2
-  allocation_id = "${aws_eip.nat.id}"
+resource "aws_nat_gateway" "ngw" {
+  count = "${local.public_subnet_count}"
+  allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.public.*.id, count.index)}"
 
   tags {
-    Name = "${local.generic_tag}"
+    Name = "${local.generic_tag}-${count.index}"
   }
 
   depends_on = ["aws_internet_gateway.igw"]
@@ -110,21 +119,23 @@ resource "aws_route_table_association" "public_subn" {
 // Private subnet routing
 //-----------------------------------------
 resource "aws_route_table" "private" {
+  count = "${local.private_subnet_count}"
   vpc_id = "${aws_vpc.main.id}"
 
   tags {
-    Name = "private-${local.generic_tag}"
+    Name = "private-${local.generic_tag}-${count.index}"
   }
 }
 
 resource "aws_route" "private" {
-  route_table_id         = "${aws_route_table.private.id}"
+  count                  = "${local.private_subnet_count}"
+  route_table_id         = "${element(aws_route_table.private.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.gw.*.id}"
+  nat_gateway_id         = "${element(aws_nat_gateway.ngw.*.id, count.index)}"
 }
 
 resource "aws_route_table_association" "private_subn" {
   count          = "${local.private_subnet_count}"
   subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
-  route_table_id = "${aws_route_table.private.id}"
+  route_table_id = "${element(aws_route_table.private.*.id, count.index)}"
 }
