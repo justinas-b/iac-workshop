f82321a3984dc3cb328d0473b606b4a533dcf709
Linas Šlepikas <linasslepikas@gmail.com>
2019-01-02 21:04:38 +0200

    Removed NGINX and added WP onboarding script via user data.

--- working-dir/user_data.sh
+++ working-dir/user_data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
-sudo amazon-linux-extras install nginx1.12 -y
-sudo systemctl enable nginx
-sudo systemctl start nginx
+sudo yum update -y
+sudo yum install -y httpd php php-mysqlnd
+sudo service httpd start
+sudo chkconfig httpd on
+cd /var/www/html/
+sudo wget http://wordpress.org/latest.tar.gz
+sudo tar xfz latest.tar.gz
+sudo mv wordpress/* ./
+sudo rmdir ./wordpress/
+sudo rm -f latest.tar.gz
