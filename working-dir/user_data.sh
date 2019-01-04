27bb4c8ee7d4912fcd14af1bb4a58cc24066e879
Linas Šlepikas <linasslepikas@gmail.com>
2019-01-04 10:30:05 +0200

    Added WP CLI tool for WP setup automation.

--- working-dir/user_data.sh
+++ working-dir/user_data.sh
@@ -10,3 +10,13 @@ sudo tar xfz latest.tar.gz
 sudo mv wordpress/* ./
 sudo rmdir ./wordpress/
 sudo rm -f latest.tar.gz
+sudo curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+php wp-cli.phar --info
+sudo chmod +x wp-cli.phar
+sudo mv wp-cli.phar /usr/local/bin/wp
+sudo cp wp-config-sample.php wp-config.php
+sudo chmod 777 wp-config.php
+wp config set DB_NAME db_name
+wp config set DB_USER db_user
+wp config set DB_PASSWORD db_password
+wp config set DB_HOST db_host
