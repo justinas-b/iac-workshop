fa4da88c3f9684df89b867b435a0114db5c8ba4f
Ignas Asmontas <ignas.asmontas@gmail.com>
2019-01-29 20:40:58 +0200

    Removed topic-4 for later workshops

--- topic-4/compute/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "john-snow-state-437278685207"
-    key    = "workshop/compute/terraform.tfstate"
-    region = "eu-central-1"
-  }
-}
--- topic-4/vpc/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "owner" {}
-variable "key_pair" {}
-variable "region" {}
-variable "network" {}
-variable "subnet_bits" {}
